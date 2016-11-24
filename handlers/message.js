fae4ff4e26beda174ec9b4b9691e7d601a44b860
appellation <nelson.will@live.com>
2016-11-24 10:59:04 -0800

    Prefix commands.

--- handlers/message.js
+++ handlers/message.js
@@ -8,7 +8,11 @@ const commands = {
 };
 
 function message(msg)   {
-    const parsed = msg.content.split(' ');
+    let parsed = msg.content.split(' ');
+
+    if(parsed[0] !== `<@${process.env.DISCORD_CLIENT_ID}>` && parsed[0] !== `<!@${process.env.DISCORD_CLIENT_ID}>`) return;
+    else parsed = parsed.slice(1);
+
     if(msg.author.bot) return;
     if(typeof commands[parsed[0]] !== 'function') return;
     commands[parsed[0]](msg.client, msg, parsed.slice(1));
