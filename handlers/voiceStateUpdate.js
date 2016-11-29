4923a99a2b1c47e0733a79b405b8674c0a079aa9
appellation <nelson.will@live.com>
2016-11-28 19:48:52 -0800

    Auto-disconnect fix.

--- handlers/voiceStateUpdate.js
+++ handlers/voiceStateUpdate.js
@@ -3,14 +3,17 @@
  */
 
 function voiceStateUpdate(oldM, newM) {
-    if(!newM.voiceChannel) return;
-    if(!newM.voiceChannel.connection) return;
-    if(newM.voiceChannel.members.size !== 0)    {
-        if(typeof newM.guild.connectionTimeout !== 'undefined') clearTimeout(newM.guild.connectionTimeout);
+    if(newM.voiceChannel && newM.voiceChannel.members.size > 1 && newM.voiceChannel.connection && typeof newM.guild.connectionTimeout !== 'undefined')    {
+        clearTimeout(newM.guild.connectionTimeout);
         return;
     }
 
-    newM.guild.connectionTimeout = setTimeout(newM.voiceChannel.connection.disconnect, 15000);
+    if(!oldM.voiceChannel || !oldM.voiceChannel.connection || oldM.voiceChannel.members.size > 1 || !newM.guild.monstercat) return;
+
+    newM.guild.connectionTimeout = setTimeout(() => {
+        newM.guild.monstercat.stop();
+        delete newM.guild.monstercat;
+    }, 15000);
 }
 
 module.exports = voiceStateUpdate;
