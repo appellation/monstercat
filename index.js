0b8f019cb7f05668223acbeeb25221c72a7de689
appellation <nelson.will@live.com>
2016-12-01 17:59:50 -0800

    unnecessary initialization

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ storage.client = client;
 client.Dispatcher.on('MESSAGE_CREATE', message_create);
 // client.Dispatcher.on('VOICE_CHANNEL_LEAVE', voice_channel_leave);
 client.Dispatcher.on('GATEWAY_READY', () => {
-    storage.monstercat = {};
     console.log('ready');
 });
 client.connect({
