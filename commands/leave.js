5bf57826e3479713630bae9057b155adf22742db
appellation <nelson.will@live.com>
2016-12-16 13:20:05 -0800

    whoops.  fix leave command.

--- commands/leave.js
+++ commands/leave.js
@@ -5,7 +5,7 @@
 const storage = require('../operators/storage');
 
 function leave(msg, args)   {
-    const mc = storage.monstercat[msg.guild.id];
+    const mc = storage.monstercat.get(msg.guild.id);
     if(!mc || !(mc instanceof require('../operators/monstercat'))) return;
     mc.stop();
     storage.monstercat.delete(msg.guild.id);
