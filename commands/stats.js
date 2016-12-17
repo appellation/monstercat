f729e48447e969630ff678fd1641d4553b0752b5
appellation <nelson.will@live.com>
2016-12-17 14:28:30 -0800

    fix stats

--- commands/stats.js
+++ commands/stats.js
@@ -5,7 +5,7 @@
 const storage = require('../operators/storage');
 
 function stats(msg, args)   {
-    msg.reply(`**Guilds:** ${storage.client.Guilds.size} (playing in ${Object.keys(storage.monstercat).length})`)
+    msg.reply(`**Guilds:** ${storage.client.Guilds.size} (playing in ${storage.monstercat.size})`)
 }
 
 module.exports = stats;
