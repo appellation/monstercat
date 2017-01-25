ccb4e38f214d832ac91ab5f2e5465c0ec6db4203
appellation <nelson.will@live.com>
2017-01-25 14:54:27 -0800

    help

---
+++ src/commands/help.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Will on 1/23/2017.
+ */
+
+exports.func = () => {
+    return `**Prefix all commands with a mention:** \`@Monstercat\`
+- **join** - Join the current voice channel and play music.
+- **leave** - Leave the current voice channel.`;
+};
