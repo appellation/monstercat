4eb122680067cc583e947354795383930899652e
appellation <nelson.will@live.com>
2017-03-08 18:44:23 -0800

    remain connected until requested (closes #14)

--- src/index.js
+++ src/index.js
@@ -50,11 +50,6 @@ client
 			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
 		`);
 	})
-	.on('voiceStateUpdate', (oldMember, newMember) => {
-		if (oldMember.voiceChannel && oldMember.voiceChannel.members.size === 1) {
-			return client.monstercat.disconnect(oldMember.guild);
-		}
-	})
 	.on('guildDelete', guild => {
 		client.monstercat.disconnect(guild);
 	});
