63d4b7be19e2e19b9814506a76c50591a1e05039
Will Nelson <nelson.will@live.com>
2017-04-14 10:31:08 -0700

    ensure that stream initialized

--- src/commands/monstercat/join.js
+++ src/commands/monstercat/join.js
@@ -15,23 +15,26 @@ module.exports = class Invite extends Command {
             }
         });
     }
-    
+
     async run(msg) {
         let voiceChan = msg.member.voiceChannel;
-        
+
         if(!voiceChan) {
             return msg.reply('you\'re not in a voice channel.');
         }
-        
+
         const botPerm = voiceChan.permissionsFor(msg.client.user);
-        
+
         if(!botPerm.hasPermission('CONNECT')) {
             return msg.reply('Insufficient Permissions. I\'m unable to join your voice channel.');
         }
         if(!botPerm.hasPermission('SPEAK')) {
             return msg.reply('Insufficient Permissions. I\'m unable to broadcast music in this channel.');
         }
-        
+        if(!this.client.monstercat) {
+            return msg.reply('Stream has not finished initializing.  Please wait.');
+        }
+
         this.client.monstercat.connect(msg.member);
     }
 };
