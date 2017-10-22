e29eed3161e9b81d470deae2f864240a7f34e7be
Will Nelson <nelson.will@live.com>
2017-10-22 15:02:57 -0700

    update deps, fix join

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -3,13 +3,14 @@ import { Command, Validator } from 'discord-handles';
 module.exports = class extends Command {
   public async pre() {
     await new Validator(this)
-      .apply(Boolean(this.client.broadcasts[0]), 'Stream has not finished initializing. Please wait...')
+      .apply(this.client.broadcasts.length > 0, 'Stream has not finished initializing. Please wait...')
       .apply(Boolean(this.member.voiceChannel), 'You\'re not in a voice channel. Please join one before using this command.');
   }
 
   public async exec() {
     const vc = this.guild.voiceConnection || await this.member.voiceChannel.join();
-    vc.playBroadcast(this.client.broadcasts[0]);
-    return this.response.success(`now streaming to \`${vc.channel.name}\``);
+    const dispatcher = vc.playBroadcast(this.client.broadcasts[0]);
+    dispatcher.setVolumeLogarithmic(0.5);
+    await this.response.success(`now streaming to \`🔊 ${vc.channel.name}\``);
   }
 }
