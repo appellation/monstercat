9b197a7873bdd11873103495a9e674a8fcf3e2a3
Will Nelson <nelson.will@live.com>
2017-10-23 10:44:29 -0700

    playing status

--- src/Client.ts
+++ src/Client.ts
@@ -1,7 +1,9 @@
 import * as path from 'path';
+import { Buffer } from 'buffer';
 
 import { config } from 'dotenv';
 import twitch = require('twitch-get-stream');
+import Websocket = require('ws');
 import { Client } from 'discord.js';
 import { Client as Handles } from 'discord-handles';
 
@@ -26,7 +28,40 @@ module.exports = new class extends Client {
   }
 
   public async init() {
-    await this.startStream();
+    this.startStream();
+
+    const connection = new Websocket('wss://irc-ws.chat.twitch.tv', 'irc');
+    connection.on('message', (message: Websocket.Data) => {
+      if (message instanceof ArrayBuffer) message = Buffer.from(message);
+      if (Array.isArray(message)) message = Buffer.concat(message);
+      if (message instanceof Buffer) message = message.toString('utf8');
+
+      console.log(message);
+
+      if (message.startsWith('PING :tmi.twitch.tv')) {
+        connection.send('PONG :tmi.twitch.tv');
+        return;
+      }
+
+      const match = message.match(/^:([^:]+) :(.+)/);
+      if (!match) return;
+
+      const [, prefix, content] = match;
+      if (prefix === 'monstercat!monstercat@monstercat.tmi.twitch.tv PRIVMSG #monstercat') {
+        const status = content.match(/^Now Playing: (.+) by (.+)/);
+        console.log(status);
+        if (!status) return;
+
+        const game = `${status[2].replace(/ - (Listen now: \S+ Tweet it: \S+|Listen on Spotify: \S+)$/, '')} - ${status[1]}`;
+        this.user.setGame(game, 'https://twitch.tv/monstercat');
+      }
+    });
+    connection.once('open', () => {
+      connection.send(`PASS ${process.env.TWITCH_OAUTH_PASSWORD}`);
+      connection.send(`NICK ${process.env.TWITCH_USERNAME}`);
+      connection.send('JOIN #monstercat');
+    });
+
     console.log('ready');
   }
 
